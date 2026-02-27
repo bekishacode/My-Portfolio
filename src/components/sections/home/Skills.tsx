da4e822ab0aaec007dd904551e6abdd9bf657350
Bereket Fikre <bekifikre19@gmail.com>
2026-02-27 11:31:58 +0300

    Add missing import for Variants

--- src/components/sections/home/Skills.tsx
+++ src/components/sections/home/Skills.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { skills } from "@/data/skills";
 import { fadeInUp, staggerContainer } from "../../../utils/animations";
 import SectionTitle from "../../ui/SectionTitle";
